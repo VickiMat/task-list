a6b5ea2b6c4a7e8a7235cc1284222652f6d3bb46
VickiMat <94490367+VickiMat@users.noreply.github.com>
2022-01-25 05:26:19 -0300

    Terminado la funcionalidad base

---
+++ src/app/components/add-task/add-task.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Task } from 'src/app/Task';
+import { UiService } from 'src/app/service/ui.service';
+import { Subscription } from 'rxjs';
+
+@Component({
+  selector: 'app-add-task',
+  templateUrl: './add-task.component.html',
+  styleUrls: ['./add-task.component.css']
+})
+export class AddTaskComponent implements OnInit {
+  @Output() onAddTask: EventEmitter<Task> = new EventEmitter
+  text:string ="";
+  day:string ="";
+  reminder:boolean = false;
+  showAddTask:boolean = false;
+  subscription?: Subscription;
+
+  constructor(
+    private uiservice: UiService
+  ) {
+    this.subscription = this.uiservice.onToggle().subscribe(value => this.showAddTask = value)
+   }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit(){
+    if(this.text.length === 0){
+      alert("PLEASE ADD A TASK!");
+      return
+    }
+
+    const{text,day,reminder} = this
+    const newTask ={text, day, reminder}
+    
+    this.onAddTask.emit(newTask);
+
+  }
+
+
+}
